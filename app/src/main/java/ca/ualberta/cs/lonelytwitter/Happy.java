b09a71d72640b165c6877636aa446bdb8d1ce55f
Taylor Folkersen <cf@Taylors-MacBook-Pro.local>
2018-01-18 19:04:53 -0700

    Should be done

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Happy.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by cf on 2018-01-18.
+ */
+
+public class Happy {
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Sad.java
@@ -0,0 +1,25 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by cf on 2018-01-18.
+ */
+
+public class Bored extends Mood {
+    @Override
+    public String toString(){
+        return "";
+    }
+
+    public Bored(Date date){
+        super(date);
+    }
+
+    public Bored(){
+        super();
+    }
+
+
+
+}
